89031e7038c6682ef96cb42a93cfd056a1c7ad2b
Juan Jesús González Hernández <jjhernan.dev@gmail.com>
2026-03-03 17:39:42 +0100

    Proyecto inicial Spring Boot.

---
+++ src/test/java/jjhernan/dev/userauth/UserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package jjhernan.dev.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
